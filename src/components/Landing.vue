d3204670784d9f07e46b62b548d156ff1f274ce3
Douglas <dz@bitzend.net>
2021-07-19 09:21:13 -0600

    git init  with working code

---
+++ src/components/Landing.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <v-container fluid>
+    <v-layout row wrap>
+
+      <v-flex xs12 class="text-xs-center" mt-5>
+      <div id="hero-section">
+        <div class="search-bar">
+          <div class="search-icon">
+            <v-img src="search-1.svg" alt="" height="15px"></v-img>
+          </div>
+          <input type="text" placeholder="Search high-resolution photos">
+        </div>
+      </div>
+      </v-flex>
+
+
+    </v-layout>
+  </v-container>
+</template>
+<script>
+export default {
+  // name: 'Landing'
+};
+</script>
